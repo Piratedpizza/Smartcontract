ad69793102ac60cda8eb02c13bd7264d4ab15218
Stonesobr <96216775+Stonesobr@users.noreply.github.com>
2021-12-16 06:50:58 +0000

    Update and rename Token.sol to ERC20FixedSupply.sol

---
+++ ERC20FixedSupply.sol
@@ -0,0 +1,10 @@
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20FixedSupply is ERC20 {
+    constructor() ERC20("Fixed", "FIX") {
+        _mint(msg.sender, 1000);
+    }
+}
